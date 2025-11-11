3c2a1caecdea8ea84a9328462249efeae43a35cd
Matt <matt.beck@eblusolutions.com>
2025-11-10 20:43:57 -0500

    rock input rules

--- ropasc.py
+++ ropasc.py
@@ -8,3 +8,12 @@ while player in options:
     break
 else:
     print("ERROR! You did not select an option!")
+    
+if player == "rock":
+    if computer == "paper":
+        print("The computer picks... paper! You lose :(")
+    elif computer == "scissors":
+        print("The computer picks... scissors! You win!!")
+    else:
+        print("The computer picks... rock! The game is a draw...")
+        
