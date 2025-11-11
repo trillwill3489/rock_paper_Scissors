111ab9225d6b0539815c9e82cd252e612178c788
Matt <matt.beck@eblusolutions.com>
2025-11-10 20:36:54 -0500

    error proof input

--- ropasc.py
+++ ropasc.py
@@ -3,4 +3,8 @@ print("Let's play rock, paper, scissors! What do you chose?")
 player = input().lower()
 options = ["rock", "paper", "scissors"]
 computer = random.choice(options)
-print(computer)
+
+while player in options:
+    break
+else:
+    print("ERROR! You did not select an option!")
